2f2de9bb2bb93ae661fdacadd19cd0c32340b981
conradogui <gui14fbce@gmail.com>
2023-09-08 17:29:50 -0300

    colocando imagens

---
+++ 3_FUNDAMENTOS/aplication/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Avançando em react</h1>
+      {/* Imagem em public */}
+      <div>
+          <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+    </>
+  )
+}
+
+export default App
