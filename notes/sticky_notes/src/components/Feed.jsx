e689c4dcd13070e953c0e980bc03b4fd44dcf4c5
conradogui <gui14fbce@gmail.com>
2024-01-31 18:36:14 -0300

    adicionando CSS ao projeto

--- notes/sticky_notes/src/components/Feed.jsx
+++ notes/sticky_notes/src/components/Feed.jsx
@@ -1,8 +1,10 @@
+import './Feed.css'
+
 const Feed = ({ taskData }) => {
     return (
-      <div>
+      <div className='itemFeed'>
         {taskData.map((task, index) => (
-          <div key={index}>
+          <div key={index} className='item'>
             <h2>{task.tarefa}</h2>
             <p>{task.data}</p>
             <p>{task.descricao}</p>
