ae01210d878a11ad1235e0bc8a1276759fe7b93e
conradogui <gui14fbce@gmail.com>
2024-05-07 19:34:25 -0300

    Estudando sobre requisições HTTP com react

--- 7_REQ_HTTP/req_http/src/App.jsx
+++ 7_REQ_HTTP/req_http/src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
+import "./App.css";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 //4 - custom hook
-import { useFetch } from './hooks/useFetch'
+import { useFetch } from "./hooks/useFetch";
 
-const url = "http://localhost:3000/products/"
+const url = "http://localhost:3000/products/";
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   //4 - custom hook
-  const {data: items, httpConfig} = useFetch(url)
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
-  const [name, setName] = useState("")
-  const [price, setPrice] = useState("")
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   //1 - resgatando dados
   // useEffect(() => {
@@ -29,12 +29,12 @@ function App() {
 
   // 2 - add de produtos
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const product = {
       name,
-      price, 
-    }
+      price,
+    };
 
     // const res = await fetch(url, {
     //   method: "POST",
@@ -42,34 +42,67 @@ function App() {
     //     "Content-Type": "application/json"
     //   },
     //   body: JSON.stringify(product),
-    // })    
+    // })
     // //3 - carregamento dinâmico
     // const addedProduct = await res.json()
     // setProducts((prevProducts) => [...prevProducts, addedProduct])
 
-    //5 -  
+    //5 - refatorando post
+    httpConfig(product, "POST");
 
-    setName("")
-    setPrice("")
+    setName("");
+    setPrice("");
+  };
+
+  //8 - desafio de botao de remover
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE")
   }
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
-      <ul>
-        {items && items.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
-        ))}
-      </ul>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados</p>}
+      {error && <p>{error}</p>}
+      {!error && (
+        <ul>
+          {items &&
+            items.map((product) => (
+              <li key={product.id}>
+                {product.name} - R$: {product.price}
+                <button onClick={() => handleRemove(product.id)}>Excluir</button>
+              </li>
+            ))}
+        </ul>
+      )}
       <div className="add-product">
         <form onSubmit={handleSubmit}>
-          <label>Nome: <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} /></label>
-          <label>Preço: <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} /></label>
-          <input type="submit" value="criar" />
+          <label>
+            Nome:{" "}
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:{" "}
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {/* 7 - state de loading no post */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="criar" />}
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- 7_REQ_HTTP/req_http/src/hooks/useFetch.jsx
+++ 7_REQ_HTTP/req_http/src/hooks/useFetch.jsx
@@ -9,25 +9,56 @@ export const useFetch = (url) => {
   const [method, setMethod] = useState(null);
   const [callFetch, setCallFetch] = useState(false);
 
+  //6 - loading
+  const [loading, setLoading] = useState(false);
+
+  // 7 - tratando erros
+  const [error, setError] = useState(null);
+
+  //8 - delete 
+  const [itemId, setItemId] = useState(null)
+
+
   const httpConfig = (data, method) => {
-    if(method === "POST") {
-        setConfig({
-            method,
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      setMethod(method);
+    } else if ( method === "DELETE") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      setMethod(method)
+      setItemId(data)
     }
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(url);
+      //6 - loading
+      setLoading(true);
 
-      const json = await res.json();
+      try {
+        const res = await fetch(url);
 
-      setData(json);
+        const json = await res.json();
+
+        setData(json);
+      } catch (error) {
+        console.log(error.message)
+
+        setError("Houve algum erro ao carregar os dados!")
+      }
+
+      setLoading(false);
     };
 
     fetchData();
@@ -36,17 +67,27 @@ export const useFetch = (url) => {
   //5 - refatorando post
   useEffect(() => {
     const httpRequest = async () => {
+
+      let json
+
       if (method === "POST") {
         let fetchOptions = [url, config];
 
         const res = await fetch(...fetchOptions);
 
-        const json = await res.json();
+        json = await res.json();
 
         setCallFetch(json);
+      } else if(method === "DELETE") {
+        const deleteUrl = `${url}/${itemId}`
+
+        const res = await fetch(deleteUrl, config)
+
+        json = await res.json()
       }
+      setCallFetch(json)
     };
-    httpRequest()
-  }, [config]);
-  return { data, httpConfig };
+    httpRequest();
+  }, [config, method, url]);
+  return { data, httpConfig, loading, error };
 };
